73d8b531dc9ad57323580dd221f6e95db60e137f
colinalford <colinalford@gmail.com>
2021-03-07 23:18:28 -0500

    Require the required things :facepalm:

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { searchCourtRecords } = require("./search-court-records");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,20 +9,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/api/court-search", (req, res) => {
-  searchCourtRecords(
-    req.body,
-    cases => res.json(cases),
-    console.log
-  );
+  searchCourtRecords(req.body, (cases) => res.json(cases), console.log);
 });
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-    
+  app.use(express.static(path.join(__dirname, "client/build")));
+
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
