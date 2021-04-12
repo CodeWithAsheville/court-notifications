49db224643a3ace86e4985129174bd717d94b8b1
Colin Alford <colinalford@gmail.com>
2021-04-11 21:24:13 -0400

    Fix workflow issues and add styles (#19)
    * Closes #1 Fix copy to clipboard behavior
    
    * Add space between button and text
    
    * Make updates to the site

---
+++ client/src/components/Footer.js
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="usa-footer__secondary-section">
+        <div className="grid-container">
+          <div className="usa-footer__logo grid-row grid-gap-2">
+            <div className="grid-col-auto">
+              <a href="https://www.codeforasheville.org">
+                <img
+                  src="code-for-asheville-logo.png"
+                  alt="Code for Asheville logo"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
