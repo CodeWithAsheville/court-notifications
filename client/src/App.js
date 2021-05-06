75a6a6547f57ab4b4a18923b32991764603d057a
Eric Jackson <ejaxon@users.noreply.github.com>
2021-05-05 20:46:41 -0400

    Updated the language on the page (#23)

--- client/src/App.js
+++ client/src/App.js
@@ -46,42 +46,54 @@ function App() {
       <ol className="usa-process-list">
         <li className="usa-process-list__item">
           <h4 className="usa-process-list__heading" ref={step1}>
-            Search For Your Case
+            Search For Your Cases
           </h4>
           <p className="margin-top-05">
-            Enter your first, middle and last name to search for your court case
+            Enter your first, middle and last name to search for your court cases.
+            They will appear in a table below.
           </p>
           <SearchForm state={state} dispatch={dispatch} />
         </li>
         <li className="usa-process-list__item">
           <h4 className="usa-process-list__heading" ref={step2}>
-            Copy File Number And Go To State's Sign Up
+            Copy File Numbers And Sign up on the NC Courts Site
           </h4>
           <p>
-            Find your cases in the table below. When you click the copy button,
-            your file number will be copied to your clipboard and the
-            notifications sign up site will open in a new tab.
+            Your cases are listed below. You must sign up for each separately.
+          </p>
+          <p>
+            When you click the copy button on a case it will copy your file number to your
+            clipboard and open the NC Courts notification signup in a new tab. Complete 
+            the signup process as described below, then close the tab to return here. The case
+            will now have a <em>Yes</em> in the <b>Signed Up?</b> column
           </p>
           <ul>
             <li>
-              You will need to finish through the state's notification sign up
-              process below.
+              <b>County</b>: Select <em>Buncombe</em> from the county selector.
             </li>
             <li>
-              Select <b>Buncombe</b> from the county picker and paste in your
-              file number
+              <b>File Number</b>: Paste in your file number into the input box.
             </li>
             <li>
-              If your search is successful, you will see your information and a
-              selector that reads "Notify Me By"
+              Click the <b>Search Case</b> button
             </li>
-            <li>You may choose to be notified by email or by text</li>
             <li>
-              You must either enter your email address or phone number, then
-              check the box to agree to the terms of service in order to
-              subscribe to any notifications
+              If your search is successful, you will see your case information and a
+              selector that reads <b>Notify Me By</b>. You may choose to be notified by
+              email or by text.
+            </li>
+            <li>
+              Enter your email address or phone number, depending on the method you chose,
+              then check the box to agree to the terms of service in order to
+              subscribe to any notifications.
+            </li>
+            <li>
+              Close the tab to return to the list of cases.
             </li>
           </ul>
+          <p>
+            Repeat this procedure until you have signed up for notifications for every case.
+          </p>
           <CaseTable state={state} dispatch={dispatch} />
         </li>
       </ol>
