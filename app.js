14831a6246d0bcc51066b3865c2154cc55530fa7
Eric Jackson <ejackson@ashevillenc.gov>
2021-06-27 12:05:14 -0400

    Fixed to read all results when paginated

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- server/search-court-records.js
+++ server/search-court-records.js
@@ -1,6 +1,9 @@
 const https = require("https");
 const { JSDOM } = require("jsdom");
 const { CourtCase } = require("./court-case")
+const axios = require('axios');
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+const tough = require('tough-cookie');
 
 function createCaseFromHtml(tr) {
   const mapped = [];
@@ -19,46 +22,83 @@ function createCaseFromHtml(tr) {
   return CourtCase(mapped);
 }
 
-function computeSearchUrl(state) {
+function computeSearchUrl(state, start, navIndex, submit) {
   let queryParams = `defendant=${state.lastName}`
 
   if (state.firstName) queryParams += `%2C${state.firstName}`
 
   if (state.middleName) queryParams += `%2C${state.middleName}`
 
-  return `https://www1.aoc.state.nc.us/www/calendars.Criminal.do?county=100&court=BTH+&${queryParams}&start=0&navindex=0&fromcrimquery=yes&submit=Search`;
+  return `https://www1.aoc.state.nc.us/www/calendars.Criminal.do?county=100&court=BTH+&${queryParams}&start=${start}&navindex=${navIndex}&fromcrimquery=yes&submit=${submit}`;
 }
 
-function searchCourtRecords(body, callback, onError) {
-  const url = computeSearchUrl(body)
+async function getCasesPage(axiosInstance, url, cases, onError) {
+  let resp = await axiosInstance.get(url, {withCredentials: true});
+  let content = resp.data;
 
-  https
-    .get(url, function (res) {
-      let content = "";
+  const dom = new JSDOM(content);
+  const table = dom.window.document.querySelector(".criminalquery-table");
 
-      res.on("data", function (chunk) {
-        content += chunk;
+  if (table) {
+    const rows = table.querySelectorAll("tbody > tr");
+    if (rows.length > 0) {
+      rows.forEach((row) => {
+        cases.push(createCaseFromHtml(row))
       });
+    }
+  }
 
-      res.on("end", function () {
-        const dom = new JSDOM(content);
+  let nextPage = {
+    keepOn: false,
+    start: 0,
+    navindex: 0,
+    submit: 'Search'
+  };
 
-        const table = dom.window.document.querySelector(".criminalquery-table");
-        const cases = []
-
-        if (table) {
-          const rows = table.querySelectorAll("tbody > tr");
-          if (rows.length > 0) {
-            rows.forEach((row) => {
-              cases.push(createCaseFromHtml(row))
-            });
+  // Check to see if there are more pages of results
+  const forms = dom.window.document.querySelectorAll("form")
+  if (forms) {
+    forms.forEach((searchagain) => {
+      if (!nextPage.keepOn) { // Skip if we already found it
+        const inputs = searchagain.querySelectorAll("input")
+        const formValues = {};
+        inputs.forEach((ip) => {
+          if (ip.getAttribute('TYPE') === 'HIDDEN') {
+            formValues[ip.getAttribute('NAME')] = ip.getAttribute('VALUE');
           }
+        });
+        if (formValues.submit === 'Next 25') {
+          nextPage.keepOn = true;
+          nextPage.start = formValues.start;
+          nextPage.navindex = formValues.navindex;
+          nextPage.submit = 'Next+25';
         }
-        
-        callback(cases)
-      });
-    })
-    .on("error", onError);
+      }
+    });
+  }
+  return nextPage
+}
+
+async function searchCourtRecords(body, callback, onError) {  
+  const axiosInstance = axios.create();
+  axiosCookieJarSupport(axiosInstance);
+  axiosInstance.defaults.jar = new tough.CookieJar(); // Make sure we're using cookies
+
+  let url = computeSearchUrl(body, 0, 0, 'Search');
+  const cases = []
+
+  let nextPage = await getCasesPage(axiosInstance, url, cases, onError, false)
+
+  let keepOn = nextPage.keepOn;
+  let count = 0;
+  while (keepOn) {
+    url = computeSearchUrl(body, nextPage.start, nextPage.navindex, nextPage.submit);
+    nextPage = await getCasesPage(axiosInstance, url, cases, onError)
+    keepOn = nextPage.keepOn;
+    count = count + 1;
+    if (count > 3) keepOn = false;
+  }
+  callback(cases);
 }
 
 module.exports = {
