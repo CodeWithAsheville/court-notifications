1a44cc1488050fdf52d4326152b671d429c1e361
Eric Jackson <ejackson@ashevillenc.gov>
2021-08-28 12:25:22 -0400

    Moving server scripts to scripts directory & adding twilio stuff

---
+++ server/scripts/notify.js
@@ -0,0 +1,33 @@
+const knexConfig = require('../knexfile');
+var env         = 'development';
+var knex        = require('knex')(knexConfig[env]);
+
+async function readyToNotify(notificationDays) {
+  let dateClause = 'court_date - CURRENT_DATE = ' + notificationDays
+
+  return await knex('cases')
+    .select('defendant_id', 'case_number', 'court_date', 'room', knex.raw('court_date - CURRENT_DATE as age'))
+    .whereRaw(dateClause)
+  .then(results => {
+    console.log(results)
+  })
+}
+
+async function notify(body, callback, onError) {  
+  let returnMessage = 'Successfully subscribed';
+
+  try {
+    // code here
+  }
+  catch (e) {
+    returnMessage = (typeof e === 'string') ? e : e.message;
+  }
+
+  callback({message: returnMessage});
+}
+module.exports = {
+  notify
+}
+
+readyToNotify(18)
+
---
+++ server/scripts/twilio/main.js
@@ -0,0 +1,16 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+14156635480',
+     to: '+17812966267'
+   })
+  .then(message => console.log(message));
+
+  
