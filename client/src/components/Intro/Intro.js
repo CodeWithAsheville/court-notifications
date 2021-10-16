3a72b2e141d1f86d5bc8932721b059bd95a19ebb
Eric Jackson <ejackson@ashevillenc.gov>
2021-10-16 11:11:57 -0400

    Minor edit

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <div className="usa-prose">
       <p>This tool allows you to sign up for text notifications about upcoming court dates in Buncombe County. It is free for use by anyone. Simply search for a name, select one of the resulting defendants, and submit with a cell phone number that can receive text messages.</p>
-      <p>This tool is created and maintained by volunteers for the benefit of the community. It is designed to provide extra support in making your court date, but no guarantee is made - showing up for court remains <em>your</em> responsibility.</p>
+      <p>This tool is created and maintained by volunteers for the benefit of the community. It is designed to provide extra support in making your court date, but no guarantee is made.Showing up for court remains <em>your</em> responsibility.</p>
     </div>
   );
 }
