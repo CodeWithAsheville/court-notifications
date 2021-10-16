31715ed0c8fe6c7b1d5656dc5c82605c49f607ff
Eric Jackson <ejackson@ashevillenc.gov>
2021-10-16 11:57:37 -0400

    More minor edits

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -3,8 +3,8 @@ import "./Intro.scss";
 export default function Intro() {
   return (
     <div className="usa-prose">
-      <p>This site lets you sign up for text message reminders about upcoming court dates in Buncombe County. It is free for use by anyone. Simply search for a name, select one of the resulting defendants, and provide a phone number that can receive text messages.</p>
-      <p>This tool is created and maintained by volunteers for the benefit of the community. It is intended to help people show up for court in Buncombe County, but no guarantee is made. Showing up for court remains <em>your</em> responsibility.</p>
+      <p>This site lets you sign up for text message reminders about upcoming criminal court dates in Buncombe County. It is free for use by anyone. Simply search for a name, select one of the resulting defendants, and provide a phone number that can receive text messages.</p>
+      <p>The site is maintained by volunteers for the benefit of the community. It is intended to help people show up for court in Buncombe County, but no guarantee is made. Showing up for court remains <em>your</em> responsibility.</p>
     </div>
   );
 }
