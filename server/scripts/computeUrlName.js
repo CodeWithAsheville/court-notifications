3fe97975e826b07d1619c586b5a8a07c9b3fba14
Eric Jackson <ejackson@ashevillenc.gov>
2021-11-21 16:49:47 -0500

    Add encodeURI on name for URL to AOC site charges lookup

--- server/scripts/computeUrlName.js
+++ server/scripts/computeUrlName.js
@@ -1,8 +1,9 @@
+
 function computeUrlName(defendant) {
   let name = defendant.last_name + ',' + defendant.first_name;
   if (defendant.middle_name) name += ',' + defendant.middle_name;
   if (defendant.suffix) name += ',' + defendant.suffix; 
-  return name;
+  return encodeURI(name);
 }
 
 module.exports = {
