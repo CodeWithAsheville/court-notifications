24877ea7e9d944b0348c662086fa52569e5ed6d0
Eric Jackson <ejackson@ashevillenc.gov>
2022-03-20 11:03:04 -0400

    Fixed another path

--- packages/server/server.js
+++ packages/server/server.js
@@ -79,7 +79,7 @@ app.post('/send-status', twilioSendStatusWebhook);
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   // Handle React routing, return all requests to React app
   app.get("*", function (req, res) {
