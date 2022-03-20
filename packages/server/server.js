a7d00f9dab9b049212ec514a4472cbd07756e4ef
Eric Jackson <ejackson@ashevillenc.gov>
2022-03-20 10:41:55 -0400

    redo path for sendfile

--- packages/server/server.js
+++ packages/server/server.js
@@ -83,7 +83,7 @@ if (process.env.NODE_ENV === "production") {
 
   // Handle React routing, return all requests to React app
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../", "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "/", "stages/client/build", "index.html"));
   });
 }
 
