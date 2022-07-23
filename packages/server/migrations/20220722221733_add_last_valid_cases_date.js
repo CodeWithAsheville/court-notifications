c87d751d4fd783818f2e47392b2abe6f9060b8b1
Eric Jackson <ejackson@ashevillenc.gov>
2022-07-23 15:02:57 -0400

    Fixed which table the date goes to - should be defendants, not subscribers

--- packages/server/migrations/20220722221733_add_last_valid_cases_date.js
+++ packages/server/migrations/20220722221733_add_last_valid_cases_date.js
@@ -1,11 +1,11 @@
 exports.up = async function(knex) {
-  await knex.schema.alterTable('subscribers', table => {
+  await knex.schema.alterTable('defendants', table => {
     table.date('last_valid_cases_date')
   });
 };
 
 exports.down = async function(knex) {
-  await knex.schema.table('subscribers', table => {
+  await knex.schema.table('defendants', table => {
     table.dropColumn('last_valid_cases_date');
   });
 };
