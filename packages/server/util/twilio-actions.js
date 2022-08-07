f937509c7b0c1cf42dd0d86cd72d5f84069a67d9
Dunsin <jesudunsinfaiye@gmail.com>
2022-08-07 21:47:10 +0100

    remove(twilio-actions.js): remove knex package

--- packages/server/util/twilio-actions.js
+++ packages/server/util/twilio-actions.js
@@ -1,47 +1,47 @@
-const { knex } = require('./db');
 const { twilioRespondToUser } = require('./twilio-respond-to-user');
 const unsubscribeImport = require('./unsubscribe');
 const doUnsubscribe = unsubscribeImport.unsubscribe;
 const { logger } = require('./logger');
 
 async function unsubscribe(req, res) {
-  // Use req.body.From to get the number, 
-  // look it up the database
-  let message = 'Successfully unsubscribed!'
+	// Use req.body.From to get the number,
+	// look it up the database
+	let message = 'Successfully unsubscribed!';
 
- try {
-    let phone = req.body.From;
-    if (phone.startsWith('+1')) {
-      phone = phone.substring(2);
-    }
-    doUnsubscribe(phone);
-  } catch(e) {
-    logger.error('twilio-actions.unsubscribe: ' + e)
-    message = 'An error occurred. Unsubscribe unsuccessful'
-  }
+	try {
+		let phone = req.body.From;
+		if (phone.startsWith('+1')) {
+			phone = phone.substring(2);
+		}
+		doUnsubscribe(phone);
+	} catch (e) {
+		logger.error('twilio-actions.unsubscribe: ' + e);
+		message = 'An error occurred. Unsubscribe unsuccessful';
+	}
 
-  twilioRespondToUser(res, message)
+	twilioRespondToUser(res, message);
 }
 
 async function resubscribe(req, res) {
-  // Use req.body.From to get the number, 
-  // look it up the database
-  let message = 'Your phone number can now receive court reminders. You will need to sign up again at https://buncombenc.courtdates.org';
+	// Use req.body.From to get the number,
+	// look it up the database
+	let message =
+		'Your phone number can now receive court reminders. You will need to sign up again at https://buncombenc.courtdates.org';
 
- try {
-    let phone = req.body.From;
-    if (phone.startsWith('+1')) {
-      phone = phone.substring(2);
-    }
-  } catch(e) {
-    logger.error('twilio-actions.resubscribe: ' + e)
-    message = 'An error occurred. Resubscribe unsuccessful.'
-  }
+	try {
+		let phone = req.body.From;
+		if (phone.startsWith('+1')) {
+			phone = phone.substring(2);
+		}
+	} catch (e) {
+		logger.error('twilio-actions.resubscribe: ' + e);
+		message = 'An error occurred. Resubscribe unsuccessful.';
+	}
 
-  twilioRespondToUser(res, message)
+	twilioRespondToUser(res, message);
 }
 
 module.exports = {
-  unsubscribe,
-  resubscribe
-}
+	unsubscribe,
+	resubscribe,
+};
