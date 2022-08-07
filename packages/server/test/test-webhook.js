456411381f57885051295c9654d187f99a6c33cd
Dunsin <jesudunsinfaiye@gmail.com>
2022-08-07 21:46:34 +0100

    remove(test-webhook.js): remove knex package

--- packages/server/test/test-webhook.js
+++ packages/server/test/test-webhook.js
@@ -1,10 +1,7 @@
 // NOTE: You need to do this with the updated path
-// for any file you want to run in isolation. 
+// for any file you want to run in isolation.
 // Yet another reason to add an actual test runner...
-require('dotenv').config({ path: '../../.env'})
-
-const knexConfig = require('../../knexfile');
-const knex = require('knex')(knexConfig);
+require('dotenv').config({ path: '../../.env' });
 
 const { twilioIncomingWebhook } = require('../twilio-incoming-webhook');
 
@@ -12,42 +9,50 @@ const { twilioIncomingWebhook } = require('../twilio-incoming-webhook');
  * We should get an actual test library...
  * But this works for now
  */
-const successess = []
-const errors = []
+const successess = [];
+const errors = [];
 
 function test(description, cb) {
-  let failed = false
+	let failed = false;
 
-  try {
-    cb()
-  } catch(e) {
-    failed = true
-    errors.push({ description, error: e })
-  }
+	try {
+		cb();
+	} catch (e) {
+		failed = true;
+		errors.push({ description, error: e });
+	}
 
-  if (!failed) successess.push({ description }) 
+	if (!failed) successess.push({ description });
 }
 /**
  * End fake test stuff
  */
 
-
 const res = {
-  writeHead: () => {},
-  end: () => {}
-}
+	writeHead: () => {},
+	end: () => {},
+};
 
 const req = {
-  body: {
-    Body: 'unsubscribe',
-    From: '+12408107724'
-  }
-}
+	body: {
+		Body: 'unsubscribe',
+		From: '+12408107724',
+	},
+};
 
-test('Should be able to parse webhook', () => twilioIncomingWebhook(req, res))
-test('What will happen?', () => (true === true))
+test('Should be able to parse webhook', () => twilioIncomingWebhook(req, res));
+test('What will happen?', () => true === true);
 
-successess.forEach(success => console.log('\x1b[32m%s\x1b[0m', success.description + '\n'))
-errors.forEach(error => console.log('\x1b[31m%s\x1b[0m', error.description + '\n\n', error.error, '\n\n'))
+successess.forEach((success) =>
+	console.log('\x1b[32m%s\x1b[0m', success.description + '\n'),
+);
+errors.forEach((error) =>
+	console.log(
+		'\x1b[31m%s\x1b[0m',
+		error.description + '\n\n',
+		error.error,
+		'\n\n',
+	),
+);
 
-process.exit()
+process.exit();
