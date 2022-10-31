c567743a709f69f7c081b1ecb67e6005126ff10d
6foot5 <6foot5@gmail.com>
2022-10-31 16:42:28 -0400

    conditionally hide elements in jail version

--- packages/client/src/components/Footer.js
+++ packages/client/src/components/Footer.js
@@ -2,6 +2,14 @@ import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
   const { t } = useTranslation();
+  let privacyStatementWithLinks = 'https://docs.google.com/document/d/19M2zCxx4gICCmjgVx1bKSHlC92Gnb2SCmkmrclLIq3Y/';
+  let privacyStatementWithoutLinks = 'https://docs.google.com/document/d/1YitpkjO2aJH2zwQ_m_Hx6NbhJHqK1q6g0OTKJkxeXAY/';
+
+  let isJailVersion = false;
+  if (window.location.hostname.includes('jail') || (typeof process.env.REACT_APP_CONTEXT !== "undefined" && process.env.REACT_APP_CONTEXT === 'jail')) {
+    isJailVersion = true;
+  }
+
   return (
     <footer>
       <div className="usa-footer__secondary-section">
@@ -16,15 +24,19 @@ export default function Footer() {
               </a>
             </div>
               <div style={{textAlign: "center", width: "100%"}}>
+                {!isJailVersion && (
+                  <span style={{paddingRight: "2em"}}>
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfraeD1Uwlz10Mr_t4mbV96Hdwb0njm-kTQofQbNjucMw8KAQ/viewform?usp=sf_link" target="_blank" rel="noreferrer">{t('footer.contact')}</a>
+                  </span>
+                )}                
                 <span style={{paddingRight: "2em"}}>
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLSfraeD1Uwlz10Mr_t4mbV96Hdwb0njm-kTQofQbNjucMw8KAQ/viewform?usp=sf_link" target="_blank" rel="noreferrer">{t('footer.contact')}</a>
+                  <a href={isJailVersion ? privacyStatementWithoutLinks : privacyStatementWithLinks} rel="noreferrer" target="_blank">{t('footer.privacy-policy')}</a>
                 </span>
-                <span style={{paddingRight: "2em"}}>
-                  <a href="https://docs.google.com/document/d/19M2zCxx4gICCmjgVx1bKSHlC92Gnb2SCmkmrclLIq3Y/" rel="noreferrer" target="_blank">{t('footer.privacy-policy')}</a>
-                </span>
+                {!isJailVersion && (
                 <span>
                   <a href="https://github.com/CodeForAsheville/court-notifications" target="_blank" rel="noreferrer">Github</a>
                 </span>
+              )}                
             </div>
           </div>
         </div>
--- packages/client/src/components/SignupForm/SignupForm.js
+++ packages/client/src/components/SignupForm/SignupForm.js
@@ -12,6 +12,11 @@ export default function SignupForm({ state, dispatch }) {
   const updatePhone = createPhoneUpdater(dispatch);
   const { t } = useTranslation();
 
+  let isJailVersion = false;
+  if (window.location.hostname.includes('jail') || (typeof process.env.REACT_APP_CONTEXT !== "undefined" && process.env.REACT_APP_CONTEXT === 'jail')) {
+    isJailVersion = true;
+  }
+
   async function doSubscription() {
     let doit = false;
     let tphone = state.phone_number;
@@ -109,11 +114,12 @@ export default function SignupForm({ state, dispatch }) {
         {t('signup.previousList')}
       </button>
       {explanationText}
-
-      <form className="usa-form lookup-form signup-form">
-        {inputBox}
-        {signupButton}
-      </form>
+      {!isJailVersion && (
+        <form className="usa-form lookup-form signup-form">
+          {inputBox}
+          {signupButton}
+        </form>
+      )}
     </div>
   );
 }
