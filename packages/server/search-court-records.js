978d1cb4e6327009be2f80d6f42a360ebaf5ed3b
Eric Jackson <ejackson@ashevillenc.gov>
2024-08-28 18:02:11 -0400

    Updated to use pg, not knex

--- packages/server/search-court-records.js
+++ packages/server/search-court-records.js
@@ -1,6 +1,8 @@
-const knexConfig = require('./cd_knexfile');
-// eslint-disable-next-line import/order
-const knex = require('knex')(knexConfig);
+const pg = require('pg');
+
+const { Client } = pg;
+
+require('dotenv').config();
 
 function sortByDefendant(a, b) {
   if (a.defendant < b.defendant) {
@@ -27,11 +29,24 @@ async function doSearch(body) {
   }
 
   let courtDateList = [];
+
+  const client = new Client({
+    host: process.env.CD_DB_HOST,
+    port: 5432,
+    user: process.env.CD_DB_USER,
+    password: process.env.CD_DB_PASSWORD,
+    database: process.env.CD_DB_NAME,
+    max: 10,
+    idleTimeoutMillis: 10000,
+  });
+  await client.connect();
   try {
-    courtDateList = await knex('criminal_dates').select('*')
-      .whereRaw('LOWER(defendant_name) LIKE ?', queryWhere.toLowerCase());
-  } catch (e) {
-    console.log('Got an error trying to read court dates ', e);
+    const res = await client.query('SELECT * FROM cn.criminal_dates WHERE LOWER(defendant_name) LIKE $1', [queryWhere.toLowerCase()]);
+    courtDateList = res.rows;
+  } catch (err) {
+    console.error('Got an error trying to read court dates ', err);
+  } finally {
+    await client.end();
   }
 
   const defendants = {};
