014e8adf4b638100f287b1bb4d0ca79dcd4808fa
Eric Jackson <ejackson@ashevillenc.gov>
2024-09-02 16:09:45 -0400

    Removed knex from unsubscribe.js

--- packages/server/util/unsubscribe.js
+++ packages/server/util/unsubscribe.js
@@ -1,30 +1,55 @@
 /* eslint-disable no-await-in-loop */
+const { getDBClient } = require('./db');
 const { logger } = require('./logger');
-const { knex } = require('./db');
 
 async function unsubscribe(phone) {
-  const subscribers = await knex('subscribers').select()
-    .where(
-      // eslint-disable-next-line comma-dangle
-      knex.raw('PGP_SYM_DECRYPT(encrypted_phone::bytea, ?) = ?', [process.env.DB_CRYPTO_SECRET, phone])
-    );
+  let client;
+  try {
+    client = getDBClient();
+    await client.connect();
+  } catch (e) {
+    logger.error(`util/subscribe.addSubscriber db connect: ${e}`);
+    throw Error('Error connecting to database');
+  }
 
   // Delete subscriber and any associated subscriptions
   try {
+    let res = await client.query(`
+      SELECT * FROM ${process.env.DB_SCHEMA}.subscribers 
+        WHERE PGP_SYM_DECRYPT(encrypted_phone::bytea, $1) = $2
+      `, [process.env.DB_CRYPTO_SECRET, phone]);
+
+    const subscribers = res.rows;
+
     for (let i = 0; i < subscribers.length; i += 1) {
       const subscriber = subscribers[i];
-      await knex('subscribers').delete().where('id', subscriber.id);
-      const subscriptions = await knex('subscriptions').where('subscriber_id', subscriber.id);
-      await knex('subscriptions').delete().where('subscriber_id', subscriber.id);
+      await client.query(`
+        DELETE FROM ${process.env.DB_SCHEMA}.subscribers WHERE id = $1
+        `, [subscriber.id]);
+      res = await client.query(`
+        SELECT * FROM ${process.env.DB_SCHEMA}.subscriptions WHERE subscriber_id = $1
+        `, [subscriber.id]);
+
+      const subscriptions = res.rows;
+
+      await client.query(`
+        DELETE FROM ${process.env.DB_SCHEMA}.subscriptions WHERE subscriber_id = $1
+        `, [subscriber.id]);
 
       // Now delete any orphaned cases and defendants
       for (let j = 0; j < subscriptions.length; j += 1) {
         const subscription = subscriptions[j];
-        await knex('defendants').where('id', subscription.defendant_id);
-        const count = await knex('subscriptions').where('defendant_id', subscription.defendant_id).count('*');
-        if (count[0].count === 0 || count[0].count === '0') {
-          await knex('cases').delete().where('defendant_id', subscription.defendant_id);
-          await knex('defendants').delete().where('id', subscription.defendant_id);
+        res = await client.query(`
+          SELECT COUNT(*) FROM ${process.env.DB_SCHEMA}.subscriptions WHERE defendant_id = $1
+          `, [subscription.defendant_id]);
+        const { count } = res.rows[0];
+        if (count === 0 || count === '0') {
+          await client.query(`
+            DELETE FROM ${process.env.DB_SCHEMA}.cases WHERE defendant_id = $1
+            `, [subscription.defendant_id]);
+          await client.query(`
+            DELETE FROM ${process.env.DB_SCHEMA}.defendants WHERE id = $1
+            `, [subscription.defendant_id]);
         }
       }
     }
