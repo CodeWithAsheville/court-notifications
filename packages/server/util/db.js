3b96ea24a9d59085521b4331514a711c62a61bd6
Eric Jackson <ejackson@ashevillenc.gov>
2024-11-08 12:04:18 -0500

    Updated db.js to also provide a plain PG client and updated notify.js to use

--- packages/server/util/db.js
+++ packages/server/util/db.js
@@ -1,7 +1,33 @@
+const pg = require('pg');
+
+const { Client } = pg;
+
+const remindersConfig = {
+  host: process.env.DB_HOST,
+  port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  max: 10,
+  idleTimeoutMillis: 10000,
+};
+
+function getClient(config) {
+  if (!config) {
+    return new Client(remindersConfig);
+  }
+  return new Client(config);
+}
+
+/*
+ * Delete this & next 3 lines plus the knex export
+ * when knex is no longer needed.
+ */
 const knexConfig = require('../knexfile');
 // eslint-disable-next-line import/order
 const knex = require('knex')(knexConfig);
 
 module.exports = {
+  getClient,
   knex,
 };
