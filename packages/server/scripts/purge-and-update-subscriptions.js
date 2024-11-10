3888c9952e18f4a6bfa143ed7c28f0d937a15769
Eric Jackson <ejackson@ashevillenc.gov>
2024-11-10 17:15:46 -0500

    Checking in latest changes

--- packages/server/scripts/purge-and-update-subscriptions.js
+++ packages/server/scripts/purge-and-update-subscriptions.js
@@ -2,13 +2,11 @@
 const i18next = require('i18next');
 const FsBackend = require('i18next-fs-backend');
 const path = require('path');
-const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+require('dotenv').config({ path: '../.env' });
 
-require('dotenv').config({ path: '../../.env' });
 const { logger } = require('../util/logger');
-const { twilioSendMessage } = require('../util/twilio-send-message');
 
-const { knex } = require('../util/db');
+const { knex, getClient } = require('../util/db');
 const { unsubscribe } = require('../util/unsubscribe');
 
 function getPreviousDate(days) {
@@ -18,17 +16,83 @@ function getPreviousDate(days) {
   return dString;
 }
 
-async function getConfigurationIntValue(name, defaultValue = 0) {
+async function getConfigurationIntValue(pgClient, name, defaultValue = 0) {
   let value = defaultValue;
-  const result = await knex('cn_configuration').select('value').where('name', '=', name);
-  if (result.length > 0) value = parseInt(result[0].value, 10);
+  try {
+    const res = await pgClient.query(`SELECT value from ${process.env.DB_SCHEMA}.cn_configuration WHERE name = $1`, [name]);
+    const results = res.rows;
+    if (results.length > 0) value = parseInt(results[0].value, 10);
+    console.log(`Got the configuration value ${name}: ${value}`);
+  } catch (err) {
+    logger.error(`Error getting configuration value ${name} client in purge-and-update-subscriptions`, err);
+    throw err;
+  }
   return value;
 }
 
-async function purgeSubscriptions() {
-  const daysBeforePurge = await getConfigurationIntValue('days_before_purge', 30);
+async function purgeSubscriptions(pgClient) {
+  const daysBeforePurge = await getConfigurationIntValue(pgClient, 'days_before_purge', 30);
   const purgeDate = getPreviousDate(daysBeforePurge);
+  // Need to add in logging ... in some detail I think. Think how we really want to do logging.
+  try {
+    // Get a list of defendants who have had no cases for long enough
+    let sql = `
+        SELECT s.defendant_id, s.subscriber_id FROM ${process.env.DB_SCHEMA}.subscriptions s
+        LEFT JOIN ${process.env.DB_SCHEMA}.defendants d on s.defendant_id = d.id
+        WHERE d.last_valid_cases_date < $1
+      `;
+    console.log('GET subs of defendants to purge');
+    console.log(sql);
+    let res = await pgClient.query(sql, [purgeDate]);
+    const subscribers = new Set();
+    const defendants = new Set();
 
+    console.log(res.rows);
+    for (let i = 0; i < res.rowCount; i += 1) {
+      // eslint-disable-next-line camelcase
+      const { subscriber_id, defendant_id } = res.rows[i];
+      subscribers.add(subscriber_id);
+      defendants.add(defendant_id);
+    }
+
+    // We can go ahead and just delete the subscriptions
+    sql = `
+        DELETE FROM ${process.env.DB_SCHEMA}.subscriptions WHERE defendant_id IN (${[...defendants].join(',')})
+    `;
+    console.log('Here is the deletion of the subscriptions');
+    console.log(sql);
+    res = await pgClient.query(sql);
+    console.log(`Deleted subscriptions: ${res.rowCount} items`);
+
+    // We can also just delete all the defendants and their cases
+    let arr = [...defendants];
+    for (let i = 0; i < arr.length; i += 1) {
+      const defendantId = arr[i];
+      res = await pgClient.query(`DELETE FROM ${process.env.DB_SCHEMA}.cases WHERE defendant_id = ${defendantId}`);
+      console.log(`Deleted cases for defendant ${defendantId}: ${res.rowCount} items `);
+      console.log(res.rows);
+      res = await pgClient.query(`DELETE FROM ${process.env.DB_SCHEMA}.defendants WHERE id = ${defendantId}`);
+      console.log(`Deleted defendant ${defendantId}: ${res.rowCount} items `, res.rows);
+      console.log(res.rows);
+    }
+
+    // Now let's look at subscribers, but deleting only if they have no other subscriptions
+    arr = [...subscribers];
+    for (let i = 0; i < arr.length; i += 1) {
+      res = pgClient.query(`SELECT * FROM ${process.env.DB_SCHEMA}.subscriptions WHERE subscriber_id = ${arr[i]}`);
+      console.log(`Checking for subscriptions for subscriber ${arr[i]}: ${res.rowCount} items`);
+      if (res.rowCount === 0) {
+        res = pgClient.query(`DELETE FROM ${process.env.DB_SCHEMA}.subscribers WHERE id = ${arr[i]}`);
+        console.log(`Deleted subscriber ${arr[i]}: ${res.rowCount} items`);
+      } else {
+        console.log(`Not deleting subscriber ${arr[i]} because they still have ${res.rowCount} subscriptions`);
+      }
+    }
+  } catch (err) {
+    logger.error('Error getting the list of defendants to be purged: ', err);
+    throw err;
+  }
+  /*
   let count = await knex('defendants').delete()
     .where('last_valid_cases_date', '<', purgeDate);
 
@@ -74,10 +138,11 @@ async function purgeSubscriptions() {
   } else {
     logger.debug('No defendants purged');
   }
+  */
 }
 
-async function updateSubscriptions() {
-  const daysBeforeUpdate = await getConfigurationIntValue('days_before_update', 7);
+async function updateSubscriptions(pgClient) {
+  const daysBeforeUpdate = await getConfigurationIntValue(pgClient, 'days_before_update', 7);
 
   if (daysBeforeUpdate < 0) return;
 
@@ -135,11 +200,20 @@ async function initTranslations() {
 // script
 (async () => {
   await initTranslations();
+  let pgClient;
+  try {
+    pgClient = getClient();
+    await pgClient.connect();
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    logger.error('Error getting database client in purge-and-update-subscriptions', err);
+    throw err;
+  }
   logger.debug('Call purgeSubscriptions');
-  await purgeSubscriptions();
+  await purgeSubscriptions(pgClient);
   logger.debug('Done with purge');
-  logger.debug('Call updateSubscriptions');
-  await updateSubscriptions();
-  logger.debug('Done with update setup');
+  // logger.debug('Call updateSubscriptions');
+  // await updateSubscriptions();
+  // logger.debug('Done with update setup');
   process.exit();
 })();
--- packages/server/testing.md
+++ packages/server/testing.md
@@ -2,10 +2,9 @@
 
 ## Notification (scripts/notify.js)
 
-The easiest way to test notifications is to change the court date in the test database and either run the notify.js script directly or, if working on the staging server, temporarily set the corresponding scheduler to run every 10 minutes.
+The easiest way to test notifications is to change the court date in the test database and either run the notify.js script directly (from inside the scripts directory) or, if working on the staging server, temporarily set the corresponding scheduler to run every 10 minutes.
 
 ```
     update cases set court_date = CURRENT_DATE + {notification days_before} where {clause to select which cases to update}
 ```
 
-Was 4pm UTC
