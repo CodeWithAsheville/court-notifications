d1a648d10f1d6e441e1d23a7ea3c98ad9eedc3af
Eric Jackson <ejackson@ashevillenc.gov>
2024-11-13 17:09:57 -0500

    Initial conversion of search-court-records

--- packages/server/search-court-records.js
+++ packages/server/search-court-records.js
@@ -1,6 +1,19 @@
-const knexConfig = require('./cd_knexfile');
-// eslint-disable-next-line import/order
-const knex = require('knex')(knexConfig);
+// const knexConfig = require('./cd_knexfile');
+// // eslint-disable-next-line import/order
+// const knex = require('knex')(knexConfig);
+
+const { getClient } = require('./util/db');
+const { logger } = require('./util/logger');
+
+const courtCasesConfig = {
+  host: process.env.CD_DB_HOST,
+  port: 5432,
+  user: process.env.CD_DB_USER,
+  password: process.env.CD_DB_PASSWORD,
+  database: process.env.CD_DB_NAME,
+  max: 10,
+  idleTimeoutMillis: 10000,
+};
 
 function sortByDefendant(a, b) {
   if (a.defendant < b.defendant) {
@@ -13,67 +26,81 @@ function sortByDefendant(a, b) {
 }
 
 async function doSearch(body) {
-  let queryWhere = `${body.lastName}`;
-  if (body.exact) {
-    if (body.firstName) queryWhere += `,${body.firstName}`;
-    if (body.middleName) queryWhere += `,${body.middleName}`;
-    if (body.suffix) queryWhere += `,${body.suffix}`;
-  } else {
-    queryWhere += '%';
-    if (body.firstName) {
-      if (body.firstName) queryWhere += `,${body.firstName}%`;
-      if (body.middleName) queryWhere += `,${body.middleName}%`;
-    }
-  }
-
-  let courtDateList = [];
-  try {
-    courtDateList = await knex('criminal_dates').select('*')
-      .whereRaw('LOWER(defendant_name) LIKE ?', queryWhere.toLowerCase());
-  } catch (e) {
-    console.log('Got an error trying to read court dates ', e);
-  }
-
-  const defendants = {};
-  courtDateList.forEach((item) => {
-    const race = (item.defendant_race && item.defendant_race.length > 0) ? item.defendant_race : '-';
-    const sex = (item.defendant_sex && item.defendant_sex.length > 0) ? item.defendant_sex : '-';
-    const defendantID = `${item.defendant_name}.${sex}.${race}`;
-    if (!(defendantID in defendants)) {
-      defendants[defendantID] = [];
-    }
-    defendants[defendantID].push(item);
-  });
-
   const cases = [];
-  const keys = Object.keys(defendants);
-  keys.forEach((item) => {
-    const caselist = defendants[item];
-    const first = caselist[0];
-    const race = (first.defendant_race && first.defendant_race.length > 0) ? first.defendant_race : '-';
-    const sex = (first.defendant_sex && first.defendant_sex.length > 0) ? first.defendant_sex : '-';
-    const d = {
-      defendant: first.defendant_name,
-      dob: '',
-      race,
-      sex,
-      cases: caselist.map((c) => {
-        const dt = c.calendar_date;
-        const x = {
-          court: (c.court_type === 'S') ? 'Superior' : 'District',
-          courtDate: `${dt.getMonth() + 1}/${dt.getDate()}/${dt.getFullYear()}`,
-          courtRoom: c.courtroom ? c.courtroom : '',
-          session: c.calendar_session,
-          caseNumber: c.case_number,
-          linkToCaseDetails: '',
-          citationNumber: c.citation_number,
-        };
-        return x;
-      }),
-    };
-    cases.push(d);
-  });
+  let pgClient;
+  try {
+    pgClient = getClient(courtCasesConfig);
+    await pgClient.connect();
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    logger.error('Error getting database client in search-court-records', err);
+    throw err;
+  }
 
+  try {
+    const schema = process.env.CD_DB_SCHEMA;
+
+    let queryWhere = `${body.lastName}`;
+    if (body.exact) {
+      if (body.firstName) queryWhere += `,${body.firstName}`;
+      if (body.middleName) queryWhere += `,${body.middleName}`;
+      if (body.suffix) queryWhere += `,${body.suffix}`;
+    } else {
+      queryWhere += '%';
+      if (body.firstName) {
+        if (body.firstName) queryWhere += `,${body.firstName}%`;
+        if (body.middleName) queryWhere += `,${body.middleName}%`;
+      }
+    }
+
+    const res = await pgClient.query(`SELECT * FROM ${schema}.criminal_dates WHERE LOWER(defendant_name) LIKE $1`, [queryWhere.toLowerCase()]);
+    // courtDateList = await knex('criminal_dates').select('*')
+    //   .whereRaw('LOWER(defendant_name) LIKE ?', queryWhere.toLowerCase());
+    const courtDateList = res.rows;
+
+    const defendants = {};
+    courtDateList.forEach((item) => {
+      const race = (item.defendant_race && item.defendant_race.length > 0) ? item.defendant_race : '-';
+      const sex = (item.defendant_sex && item.defendant_sex.length > 0) ? item.defendant_sex : '-';
+      const defendantID = `${item.defendant_name}.${sex}.${race}`;
+      if (!(defendantID in defendants)) {
+        defendants[defendantID] = [];
+      }
+      defendants[defendantID].push(item);
+    });
+
+    const keys = Object.keys(defendants);
+    keys.forEach((item) => {
+      const caselist = defendants[item];
+      const first = caselist[0];
+      const race = (first.defendant_race && first.defendant_race.length > 0) ? first.defendant_race : '-';
+      const sex = (first.defendant_sex && first.defendant_sex.length > 0) ? first.defendant_sex : '-';
+      const d = {
+        defendant: first.defendant_name,
+        dob: '',
+        race,
+        sex,
+        cases: caselist.map((c) => {
+          const dt = c.calendar_date;
+          const x = {
+            court: (c.court_type === 'S') ? 'Superior' : 'District',
+            courtDate: `${dt.getMonth() + 1}/${dt.getDate()}/${dt.getFullYear()}`,
+            courtRoom: c.courtroom ? c.courtroom : '',
+            session: c.calendar_session,
+            caseNumber: c.case_number,
+            linkToCaseDetails: '',
+            citationNumber: c.citation_number,
+          };
+          return x;
+        }),
+      };
+      cases.push(d);
+    });
+  } catch (err) {
+    console.log('Error in search-court-records.js: ', err)
+  } finally {
+    await pgClient.end();
+  }
   return cases.sort(sortByDefendant);
 }
 
