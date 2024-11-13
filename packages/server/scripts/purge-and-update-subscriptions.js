12da033c19d759631976a6d69849f8ca4738e6df
Eric Jackson <ejackson@ashevillenc.gov>
2024-11-13 07:10:52 -0500

    delete debug statement

--- packages/server/scripts/purge-and-update-subscriptions.js
+++ packages/server/scripts/purge-and-update-subscriptions.js
@@ -112,7 +112,7 @@ async function updateSubscriptions(pgClient) {
     `SELECT id FROM ${schema}.defendants WHERE updated_at < $1 AND flag <> 1`,
     [updateDate],
   );
-  console.log('Row count is ', res.rowCount);
+
   if (res.rowCount > 0) {
     const defendantsToUpdate = res.rows.map(({ id }) => id);
 
