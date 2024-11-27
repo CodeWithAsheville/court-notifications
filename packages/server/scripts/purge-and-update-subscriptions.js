169e45fa0bc1a1f8526f398846bbd3e048b8b72b
Eric Jackson <ejackson@ashevillenc.gov>
2024-11-27 18:24:25 -0500

    Cleaning up

--- packages/server/scripts/purge-and-update-subscriptions.js
+++ packages/server/scripts/purge-and-update-subscriptions.js
@@ -63,7 +63,6 @@ async function purgeSubscriptions(pgClient) {
 
           // Attempt to notify them
           if (subscribers && subscribers.length > 0) {
-            logger.debug(`${subscribers.length} subscribers purged`);
             for (let j = 0; j < subscribers.length; j += 1) {
               const s = subscribers[j];
               try {
@@ -83,21 +82,19 @@ async function purgeSubscriptions(pgClient) {
           for (let j = 0; j < subscribers.length; j += 1) {
             const subscriberId = subscribers[j].subscriber_id;
             const phone4 = subscribers[j].phone.substring(subscribers[j].phone.length - 4);
+
             // Log it
-            console.log(`Unsubscribe ${phone4} for ${d.long_id}`);
             sql = `
               INSERT INTO ${schema}.log_unsubscribes
                (phone4, long_id, original_subscribe_date, last_valid_cases_date)
                VALUES ($1, $2, $3, $4) RETURNING phone4
             `;
-            console.log(sql);
             res = await pgClient.query(
               sql,
               [phone4, d.long_id, subscribers[j].created_at, d.last_valid_cases_date],
             );
-            console.log('did it ', res.rows);
+
             // Do it
-            console.log('Now do it');
             sql = `SELECT * FROM ${schema}.subscriptions WHERE subscriber_id = ${subscriberId}`;
             res = await pgClient.query(sql);
             if (res.rowCount === 0) {
@@ -191,7 +188,6 @@ async function initTranslations() {
     pgClient = getClient();
     await pgClient.connect();
   } catch (err) {
-    // eslint-disable-next-line no-console
     logger.error('Error getting database client in purge-and-update-subscriptions', err);
     throw err;
   }
