0a89e59cbe2001ee8ae5f3327a721c4e9015707c
Eric Jackson <ejackson@ashevillenc.gov>
2024-11-29 11:32:29 -0500

    Adding logging to unsubscribe function + fixing bug

--- packages/server/util/unsubscribe.js
+++ packages/server/util/unsubscribe.js
@@ -21,25 +21,42 @@ async function unsubscribe(phone, reason, dbClientIn) {
       [process.env.DB_CRYPTO_SECRET, phone],
     );
     if (res.rowCount > 0) { // Otherwise there's nothing to do
-      const subscriberId = res.rows[0].id;
+      const { id } = res.rows[0];
+      const phone4 = phone.substring(phone.length - 4);
+
       pgClient.query('BEGIN');
       try {
-        res = await pgClient.query(`SELECT defendant_id FROM ${schema}.subscriptions WHERE subscriber_id = $1`, [subscriberId]);
-        if (res.rowCount > 0) {
-          const defendants = res.rows.map((obj) => obj.defendant_id);
-          for (let i = 0; i < defendants.length; i += 1) {
-            res = await pgClient.query(
-              `SELECT COUNT (*) FROM ${schema}.subscriptions WHERE defendant_id = $1`,
-              [defendants[i]],
-            );
-            if (res.rowCount === 1) { // Delete if this is the only subscriber
-              await pgClient.query(`DELETE FROM ${schema}.cases WHERE defendant_id = $1`, [defendants[i]]);
-              await pgClient.query(`DELETE FROM ${schema}.defendants WHERE id = $1`, [defendants[i]]);
-            }
+        res = await pgClient.query(
+          `
+            SELECT s.defendant_id, s.created_at as original_subscribe_date, d.long_id, d.last_valid_cases_date
+             FROM ${schema}.subscriptions s LEFT JOIN ${schema}.defendants on s.defendant_id = d.id
+             WHERE s.subscriber_id = $1
+          `,
+          [id],
+        );
+        const defendants = res.rows;
+        for (let i = 0; i < defendants.length; i += 1) {
+          const d = defendants[i];
+          res = await pgClient.query(
+            `SELECT COUNT(*) FROM ${schema}.subscriptions WHERE defendant_id = $1`,
+            [d.defendant_id],
+          );
+          if (res.rows[0] === 1) { // Delete if this is the only subscriber
+            await pgClient.query(`DELETE FROM ${schema}.cases WHERE defendant_id = $1`, [defendants[i]]);
+            await pgClient.query(`DELETE FROM ${schema}.defendants WHERE id = $1`, [defendants[i]]);
           }
+          // Log it
+          res = await pgClient.query(
+            `
+              INSERT INTO ${schema}.log_unsubscribes
+              (phone4, long_id, reason, original_subscribe_date, last_valid_cases_date)
+              VALUES ($1, $2, $3, $4, $5)
+            `,
+            [phone4, d.long_id, reason, d.original_subscribe_date, d.last_valid_cases_date],
+          );
         }
-        await pgClient.query(`DELETE FROM ${schema}.subscriptions WHERE subscriber_id = $1`, [subscriberId]);
-        await pgClient.query(`DELETE FROM ${schema}.subscribers WHERE id = $1`, [subscriberId]);
+        await pgClient.query(`DELETE FROM ${schema}.subscriptions WHERE subscriber_id = $1`, [id]);
+        await pgClient.query(`DELETE FROM ${schema}.subscribers WHERE id = $1`, [id]);
         pgClient.query('COMMIT');
       } catch (err) {
         logger.error(`Error unsubscribing phone ending in ${phone.substring(phone.length - 4)} - rolling back: ${err}`);
