cf9e6a3ab44a6eb1efad94cbc7f37de7c3108215
Eric Jackson <ejackson@ashevillenc.gov>
2024-12-18 15:59:56 -0500

    Debug

--- packages/server/get-configuration.js
+++ packages/server/get-configuration.js
@@ -2,6 +2,7 @@ const { getClient } = require('./util/db');
 const { logger } = require('./util/logger');
 
 async function getConfiguration(query, callback) {
+  console.log('Getting configuration: ', query);
   const returnValue = {
     name: query.name,
     type: null,
@@ -20,6 +21,7 @@ async function getConfiguration(query, callback) {
     try {
       const res = await pgClient.query(`SELECT * FROM ${process.env.DB_SCHEMA}.cn_configuration WHERE name = $1`, [query.name]);
       const config = res.rows;
+      console.log('Result: ', config);
       if (config.length > 0) {
         returnValue.type = config[0].type;
         returnValue.value = config[0].value;
@@ -33,6 +35,7 @@ async function getConfiguration(query, callback) {
       await pgClient.end();
     }
   }
+  console.log('returnValue = ', returnValue);
   callback(returnValue);
 }
 
