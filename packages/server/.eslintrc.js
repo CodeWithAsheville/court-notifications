c502173eb504ea5d5d456d102639f993f4e2cb6c
Eric Jackson <ejackson@ashevillenc.gov>
2025-12-17 11:42:26 -0500

    Reverting the last few changes to regroup

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
         "es2021": true
     },
     "extends": [
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "airbnb"
     ],
     "overrides": [
     ],
