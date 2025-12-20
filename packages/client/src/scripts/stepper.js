875b72668382a032c3a3aa600b88b2295746a8c8
Eric Jackson <ejackson@ashevillenc.gov>
2025-12-20 18:05:04 -0500

    remove debugging

--- packages/client/src/scripts/stepper.js
+++ packages/client/src/scripts/stepper.js
@@ -3,13 +3,10 @@ function Stepper() {
 
   const setSteps = (newSteps) => (steps = newSteps);
 
-  const scrollToStep = (index) => {
-    console.log('Index is ', index);
-    console.log('That means ', steps[index-1]);
+  const scrollToStep = (index) =>
     steps[index - 1].current.scrollIntoView({
       behavior: "smooth",
-    })
-  };
+    });
 
   return {
     setSteps,
