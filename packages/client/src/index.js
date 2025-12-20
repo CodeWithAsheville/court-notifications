622fe251e836717f91c3dcedac90f1f40652af6e
Eric Jackson <ejackson@ashevillenc.gov>
2025-12-20 15:58:07 -0500

    Upgrade various client components

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -5,6 +5,16 @@ import App from './App';
 import './i18n';
 import reportWebVitals from './reportWebVitals';
 
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+  <Suspense fallback="loading">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>
+);
+/*
 ReactDOM.render(
   <Suspense fallback="loading">
   <React.StrictMode>
@@ -13,7 +23,7 @@ ReactDOM.render(
   </Suspense>,
   document.getElementById('root')
 );
-
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
