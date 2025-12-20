6872d76e91c8d11c2133eee02974083792987fa2
Eric Jackson <ejackson@ashevillenc.gov>
2025-12-20 16:01:34 -0500

    Finished upgrade to React 18.0.0

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -14,16 +14,6 @@ root.render(
     </React.StrictMode>
   </Suspense>
 );
-/*
-ReactDOM.render(
-  <Suspense fallback="loading">
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Suspense>,
-  document.getElementById('root')
-);
-*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
